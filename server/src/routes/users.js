b73685057bf80b886bebab7ca34665043374c4c5
Danny <dannyputraht@gmail.com>
2020-09-21 23:39:48 +0700

    modifed export as routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,10 +1,10 @@
-import express from 'express'
-
+const express = require("express");
 const router = express.Router()
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
+    res.send('respond with a resource')
 })
 
-export default router
+
+module.exports = router;
