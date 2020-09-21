869e4e1dfb9a800d1b8620bd07ca2ab820783a3c
Danny <dannyputraht@gmail.com>
2020-09-21 23:40:54 +0700

    simplify app.js for routes and initial db

--- server/app.js
+++ server/app.js
@@ -8,42 +8,37 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-import indexRouter from './src/routes/index'
-import usersRouter from './src/routes/users'
-
-import db from './src/models'
-
 const app = express()
+
+// startup tasks
+require('./src/startup/db');
+require('./src/startup/routes')(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'src', 'views'))
 app.set('view engine', 'jade')
-
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-db.sequelize.sync()
-
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+// db.sequelize.sync()
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
+    next(createError(404))
 })
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = process.env.NODE_ENV === 'development' ? err : {}
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = process.env.NODE_ENV === 'development' ? err : {}
 
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
 })
 
 export default app
