29758eb378eb2eb110e3af2bd59f0721c188d5ae
zuhri <muhammad.zuhri@greenhouse.co>
2020-09-22 02:04:19 +0700

    implement error handler

---
+++ server/src/middlewares/errorHandler.js
@@ -0,0 +1,37 @@
+const {
+  AlreadyExistError,
+  NotFoundError,
+  ForbiddenError,
+  UnauthorizedError,
+  ExternalError
+} = require('../utils/exception')
+
+const responseJSON = (err, message) => ({
+  success: false,
+  message,
+  data: err
+})
+
+const handler = (err, req, res, next) => {
+  if (err instanceof NotFoundError) {
+    res.status(404).send(responseJSON(err, 'Not Found'))
+    next(err)
+  } else if (err instanceof ForbiddenError) {
+    res.status(403).send(responseJSON(err, 'Forbidden'))
+    next(err)
+  } else if (err instanceof UnauthorizedError) {
+    res.status(401).send(responseJSON(err, 'Unauthorize'))
+    next(err)
+  } else if (err instanceof AlreadyExistError) {
+    res.status(409).send(responseJSON(err, 'Already Exist'))
+    next(err)
+  } else if (err instanceof ExternalError) {
+    res.status(503).send(responseJSON(err, 'External Service Unavailable'))
+    next(err)
+  } else {
+    res.status(500).send(responseJSON(err, 'Internal Server Error'))
+    next(err)
+  } // pass error on if not a validation error
+}
+
+module.exports = { handler }
