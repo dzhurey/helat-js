e4df3d21d3b65700053bf2919ac46f5f1b21b256
zuhri <muhammad.zuhri@greenhouse.co>
2020-09-22 02:05:16 +0700

    test sequelize model in routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import models from '../models'
 
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
+router.get('/', async function (req, res, next) {
+  const users = await models.User.findAll()
+  res.send(users)
 })
 
 export default router
