f9aed88beb2e01a975b4a7dc7683d16ceedd4171
sena <ngurah.susena@perdana-consulting.co.id>
2021-01-21 23:06:04 +0700

    login page

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import ForgotPassword from '../views/ForgotPassword.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/forgot',
+    name: 'ForgotPassword',
+    component: ForgotPassword
   },
   {
     path: '/about',
---
+++ client/src/views/ForgotPassword.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="vue-template">
+        <form novalidate v-on:submit.prevent="submit">
+            <h3 class="text-center">Forgot Password</h3>
+
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control form-control-lg" v-model="email" />
+            </div>
+
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Resend Password</button>
+
+            <div class="social-icons">
+                <ul>
+                    <li><a href="#"><i class="fa fa-google"></i></a></li>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                </ul>
+            </div>
+
+        </form>
+    </div>
+</template>
